f6ab14da7ff7959823d6995a551b22060868cdfa
Kathleen Elie <kath@MacBook-Air-de-Kathleen.local>
2018-09-28 21:29:50 +0200

    rajoute fonts et scripts

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+ setTimeout(function AlertIt() {
+var answer = confirm ("Hey le site xxxvidsxxx est trop bien")
+if (answer)
+window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}, 1000);
+
